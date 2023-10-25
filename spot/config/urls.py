6bb94a138e9afc5a292f2bd82349a1f93354095e
qnaeub <ha_nasol@naver.com>
2023-10-25 14:26:27 +0900

    url fix example #1

--- spot/config/urls.py
+++ spot/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('parking/', include('parking.urls')),
+    path('', include('parking.urls')),
 ]
